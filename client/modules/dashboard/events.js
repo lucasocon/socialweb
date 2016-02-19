c532d8ee9c39edf8b80dc523806871f25e0bbb67
Lucas Ocon <lucas_masterclas@hotmail.com>
2016-02-18 21:37:07 -0300

    add search website feature

--- client/modules/dashboard/events.js
+++ client/modules/dashboard/events.js
@@ -15,18 +15,27 @@ Template.dashboard.events({
 
 Template.website_add_form.events({
   'submit .js-add-website': function (event) {
-     var url, description;
-     url = event.target.url.value;
-     description = event.target.description.value;
+    var url, description;
+    url = event.target.url.value;
+    description = event.target.description.value;
 
-     if (Meteor.user()) {
-       Websites.insert({
+    if (Meteor.user()) {
+      Websites.insert({
         url: url,
         description: description,
         votes: 0,
         createdOn: new Date(),
         createdBy: Meteor.user()._id
-       });
-     }
+      });
+    }
   }
-})
+});
+
+Template.search_bar.events({
+  'submit .js-search-website': function (event) {
+    var search = event.target.search.value;
+    Session.set('searchFilter', search);
+
+    return false;
+  }
+});
--- client/modules/dashboard/helpers.js
+++ client/modules/dashboard/helpers.js
@@ -1,5 +1,16 @@
 Template.dashboard.helpers({
   websites: function() {
-    return Websites.find({}, {sort: {votes: -1}});
+    if (Session.get('searchFilter')) {
+      var results = Websites.find({url: Session.get('searchFilter')}, {sort: {votes: -1}});
+      if (results.count() == 0) {
+        return ['Sorry not websites found'];
+      }
+      else {
+        return results;
+      }
+    }
+    else {
+      return Websites.find({}, {sort: {votes: -1}});
+    }
   }
 });
